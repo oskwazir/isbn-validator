97bc826610a95afd1a0da63b43fbf7c64c4b61be
Omer Wazir <omer.wazir@gmail.com>
2015-01-18 10:07:55 -0700

    Renamed file for simplicity

--- test/validator.js
+++ test/validator.js
@@ -2,41 +2,34 @@ var test = require('tape');
 var isbnValidator = require('../index');
 
 test('should ignore dashes in isbn code', function (t){
-  t.plan(2);
-
   t.equal(isbnValidator("04-706-657-69"), true);
   t.equal(isbnValidator("047---06-6576-9"), true);
-  
+  t.end();
 })
 
 test('get true for valid isbn', function (t) {
-    t.plan(10);
-
     t.equal(isbnValidator("032157351X"), true);
     t.equal(isbnValidator("0262033844"), true);
     t.equal(isbnValidator("0-7475-3269-9"), true);
     t.equal(isbnValidator("156881111X"), true);
     t.equal(isbnValidator("1565923065"), true);
-
     t.equal(isbnValidator("059600298X"), true);
     t.equal(isbnValidator("193-6-493934"), true);
     t.equal(isbnValidator("0-7475-3269-9"), true);
     t.equal(isbnValidator("0-----714862533"), true);
     t.equal(isbnValidator("0789434806"), true);
+    t.end();
 });
 
-test('get false if isbn is not a string', function (t) {
-    t.plan(4);
-
+test('get false if isbn is not a string', function (t) {   
     t.equal(isbnValidator(015), false);
     t.equal(isbnValidator(["abc"]), false);
     t.equal(isbnValidator({isbn:"4050"}), false);
     t.equal(isbnValidator([0,2,62,03,38,44]), false);
+    t.end();
 });
 
 test('get false if isbn is not valid', function (t) {
-    t.plan(10);
-
     t.equal(isbnValidator("01234567"), false);
     t.equal(isbnValidator("01234567","0123456789XXX"), false);
     t.equal(isbnValidator("0123456789XXX"), false);
@@ -47,6 +40,5 @@ test('get false if isbn is not valid', function (t) {
     t.equal(isbnValidator("15681111Y"), false);
     t.equal(isbnValidator("B00HLT0YT0"), false);
     t.equal(isbnValidator("01234567"), false);
-
-    
+    t.end();
 });
